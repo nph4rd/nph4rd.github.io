24e99fdb09583928b9b76e565ca3d1b251377918
Arturo Marquez <arturomf94@gmail.com>
2020-05-27 19:21:31 -0500

    Add line breaks in ipfs hashes

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ currentDate=`date`
 sed -i '$d' ipfs.html
 echo "<hr>" >> ipfs.html
 echo $currentDate >> ipfs.html
-ipfs add --ignore-rules-path=.ipfsignore -r . >> ipfs.html
+ipfs add --ignore-rules-path=.ipfsignore -r . >> tmp_hashes.txt
+sed -i -e 's/added/<br>added/g' tmp_hashes.txt
+cat tmp_hashes.txt >> ipfs.html
+rm tmp_hashes.txt
 echo "</html>" >> ipfs.html
 git add -A
 git commit -a -m 'Update ipfs hashes'
